b41a032a790ba7095d23193b8492695fd9a88b21
lubomyr <lubomyr31@gmail.com>
2016-05-29 07:01:16 +0300

    updated script

--- db_script.sql
+++ db_script.sql
@@ -3,8 +3,8 @@ INSERT INTO `COMPANIES` (`id`, `name`, `estimateEarnings`, `parent_id`) VALUES
   (2, 'Apple', 900, NULL),
   (3, 'Microsoft', 800, NULL),
   (4, 'Skype', 700, 3),
-  (5, 'Angular', 600, 1),
-  (6, 'SoftServe', 500, 5),
-  (7, 'GlobalLogic', 400, 3),
+  (5, 'Android', 600, 1),
+  (6, 'Angular', 500, 5),
+  (7, 'Nokia', 400, 3),
   (8, 'Facebook', 300, 4);
 
