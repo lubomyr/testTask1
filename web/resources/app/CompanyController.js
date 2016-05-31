996704117058336f2ec01e6d2b75a98e37593e4d
lubomyr <lubomyr31@gmail.com>
2016-05-31 23:53:37 +0300

    Removed unused code in CompanyController

--- web/resources/app/CompanyController.js
+++ web/resources/app/CompanyController.js
@@ -11,20 +11,6 @@ mainModule.controller('CompanyController', ['$scope', '$uibModal', 'CompanyServi
             location.reload();
         };
 
-        $scope.getAllCompanies = function () {
-            companyService.getAllCompanies().then(function (data) {
-                $scope.companies = data;
-            });
-        };
-
-        $scope.getAllCompanies();
-
-        $scope.getCompanyById = function (companyId) {
-            companyService.getListAllChannelsById(companyId).then(function (data) {
-                $scope.company = data;
-            });
-        };
-
         $scope.getRootCompanies = function () {
             companyService.getRootCompanies().then(function (data) {
                 $scope.rootCompanies = data;
@@ -32,18 +18,6 @@ mainModule.controller('CompanyController', ['$scope', '$uibModal', 'CompanyServi
         };
 
         $scope.getRootCompanies();
-
-        $scope.getCompaniesWithChildren = function (companyId) {
-            companyService.getCompaniesWithChildren(companyId).then(function (data) {
-                $scope.companiesWithChildren = data;
-            });
-        };
-
-        $scope.getCompaniesWithParent = function (companyId) {
-            companyService.getCompaniesWithParent(companyId).then(function (data) {
-                $scope.companiesWithParent = data;
-            });
-        };
 
         $scope.addNewCompany = function (company) {
             console.log("add new Company :" + company);
